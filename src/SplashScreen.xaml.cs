980c605e38a655b1f114789a6a6ad5cdd10b508a
feliphechaves <chavesfeliphe@gmail.com>
2024-12-23 23:32:21 -0300

    add config launcher

--- src/SplashScreen.xaml.cs
+++ src/SplashScreen.xaml.cs
@@ -18,7 +18,7 @@ namespace CanaryLauncherUpdate
 {
 	public partial class SplashScreen : Window
 	{
-		static string launcerConfigUrl = "https://github.com/feliphechaves/aioloslauncher/blob/main/launcher_config.json";
+		static string launcerConfigUrl = "https://raw.githubusercontent.com/feliphechaves/aioloslauncher/refs/heads/main/launcher_config.json";
 		// Load informations of launcher_config.json file
 		static ClientConfig clientConfig = ClientConfig.loadFromFile(launcerConfigUrl);
 
